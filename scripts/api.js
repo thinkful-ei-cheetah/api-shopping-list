0cdb2e239b2806fd54814302bad75fb1fb610139
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-03 12:05:38 -0400

    new item creation via api

--- scripts/api.js
+++ scripts/api.js
@@ -5,19 +5,19 @@ const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/verdi';
  
   const getItems = function(){
-    
-    return fetch(`${BASE_URL}/items`)
+    return fetch(`${BASE_URL}/items`);
   };
 
   const createItem = function(name){
-    const newItem = {
-      name
-    };
-
+    const newItem = {name};
     const stringifiedItem = JSON.stringify(newItem);
 
-    return fetch(`${BASE_URL}/items`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: stringifiedItem})
-  }
+    return fetch(`${BASE_URL}/items`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: stringifiedItem
+    });
+  };
   
   return {
     getItems,
