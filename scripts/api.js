0606af9a6ef353a9a7297768a6c4ffa20c78d212
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-04-03 08:37:31 -0700

    completed 2-4

--- scripts/api.js
+++ scripts/api.js
@@ -5,11 +5,23 @@ const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/verdi';
  
   const getItems = function(){
-    return Promise.resolve('it was successful');
+    
+    return fetch(`${BASE_URL}/items`)
   };
+
+  const createItem = function(name){
+    const newItem = {
+      name
+    };
+
+    const stringifiedItem = JSON.stringify(newItem);
+
+    return fetch(`${BASE_URL}/items`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: stringifiedItem})
+  }
   
   return {
-    getItems
+    getItems,
+    createItem
   };
 
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,3 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
-
-api.getItems()
-  .then(res => console.log(res));
-
-console.log(api.BASE_URL);
